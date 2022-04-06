b64245d7ad0bd4c7a66e8b90af5c64a2346e47a6
NTSK <nit.ipod@gmail.com>
2022-04-07 02:21:04 +0900

    Update

--- cbREV-IO-Emu/cbREV-IO-Emu.ino
+++ cbREV-IO-Emu/cbREV-IO-Emu.ino
@@ -1,10 +1,11 @@
-//cbREV MAIN IO Emulation
-//Arduino Nanoで動作確認しています。動作の保証はしません。酒駆動式開発ですので乱雑です。
-//ピン配列等はBlogへ。Nu1.1 CN4 : 115200bps 8N1 UART (EIA232Level) 3:Nu1.1 RX 5:Nu1.1 TX 9:GND です。
+// REV MAIN IO Imitator
+// Arduino Nanoで動作確認しています。動作の保証はしません。酒駆動式開発ですので乱雑です。
+//ピン配列等はBlogへ。PCB CN4 : 115200bps 8N1 UART (EIA232Level) 3:PCB RX 5:PCB TX 9:GND です。
 
-//良きに232ドライバをNu1.1のCN4 UART2(上記)に繋いで、このスケッチを書き込んだArduinoなどのTXD/RXDをドライバにいい感じに接続すれば動きます。
+//良きに232ドライバをPCBのCN4 UART2(上記)に繋いで、このスケッチを書き込んだArduinoなどのTXD/RXDをドライバにいい感じに接続すれば動きます。
 
 // 2021/03/11 NTSK-lab. Pray Tohoku.
+// 2022/4/7 Update
 
 String inputString = "";
 String outputString = "";
@@ -23,14 +24,14 @@ int initCnt = 0;
 
 void setup()
 {
-    //Nu1.1 CN4 : 115200bps 8N1 UART (EIA232Level) 3:Nu1.1 RX 5:Nu1.1 TX 9:GND
+    // PCB CN4 : 115200bps 8N1 UART (EIA232Level) 3:PCB RX 5:PCB TX 9:GND
     Serial.begin(115200);
 
     inputString.reserve(256);
     outputString.reserve(256);
 
     pinMode(LED_BUILTIN, OUTPUT);
-    pinMode(WORKLED_328P,OUTPUT);
+    pinMode(WORKLED_328P, OUTPUT);
     pinMode(TESTBUTTON, INPUT_PULLUP);
 }
 
@@ -40,9 +41,9 @@ void loop()
     {
         if (isInit)
         {
-            //Initが終わっている:ゲーム起動中
+            // Initが終わっている:ゲーム起動中
 
-            // Nu1.1でリセットする取扱（例：テストを抜ける、定期4:00の再起動）をするとInitの/iが来る
+            // PCBでリセットする取扱（例：テストを抜ける、定期4:00の再起動）をするとInitの/iが来る
             if (inputString.startsWith("/i"))
             {
                 isInit = false;
@@ -53,8 +54,8 @@ void loop()
             // /s(小文字)がInit終わったあと一発目に来る
             else if (inputString.startsWith("/s"))
             {
-                //CircLink基板から通信を始める。なんか一発目は内容がちょっと違った。
-                //O:ってなに。
+                // IO基板から通信を始める。なんか一発目は内容がちょっと違った。
+                // O:ってなに。
                 outputString = "/S\n:V,0210\nST:1\nCO:F,0\nSW:0,0,0\nO:0\n/E\n";
             }
             else //定常通信
@@ -62,7 +63,7 @@ void loop()
                 if (inputString.startsWith("/S"))
                 {
                     /*
-                    CH9 Nu -> CircLink
+                    CH9 PCB -> IO
                         /S
                         :314776
                         CO:F,0
@@ -77,8 +78,7 @@ void loop()
 
                     if (inputString.startsWith("CO:F,0"))
                     {
-                        //ゲームモード
-                        //本来はコインブロッカー他を動かすのに用いる？のかな？
+                        //コインブロッカーON
                         //使い道ないのでLEDでも点滅させておく。高速で見えないけど。
                         digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
                         digitalWrite(WORKLED_328P, !digitalRead(WORKLED_328P));
@@ -87,7 +87,7 @@ void loop()
                     }
                     else if (inputString.startsWith("CO:T,0"))
                     {
-                        //テストモード
+                        //コインブロッカーOFF
                         //同様に使いみち無いのでLEDを点灯状態にする
                         digitalWrite(LED_BUILTIN, HIGH);
                         digitalWrite(WORKLED_328P, HIGH);
@@ -111,32 +111,41 @@ void loop()
 
                 if (digitalRead(TESTBUTTON) == LOW)
                 {
-                    //全部1にしたらTESTになった(他のボタンは不明、ここではない？説あり。)
-                    switchString = "SW:1,1,1\n";
+                    // 1要素目 現在押されているボタン
+                    // 2要素目 今回の通信で新たに押されたボタン
+                    // 3要素目 今回の通信で新たに離されたボタン
+                    if (switchString.startsWith("SW:1,1,0"))
+                        switchString = "SW:1,0,0\n";
+                    else
+                        switchString = "SW:1,1,0\n";
+                }
+                else if (switchString.startsWith("SW:1,1,0"))
+                {
+                    switchString = "SW:0,0,1\n";
                 }
                 else
                 {
                     switchString = "SW:0,0,0\n";
                 }
 
-                //outputStringの組み立て
-                outputString = "/S\n:V,0210\nST:3\n"; //本当はTEST入ってしばらくするとST:1になるけどまぁ・・・・大丈夫っしょ・・・
+                // outputStringの組み立て
+                outputString = "/S\n:V,0210\nST:3\n";
                 outputString.concat(statusString);
                 outputString.concat(switchString);
-                outputString.concat("VO:0\nAV:"); //VO : Volume Rotary Encoder?
+                outputString.concat("VO:0\nAV:"); // VO : ボリュームR/Eが動いている間1になる？
                 outputString.concat(audioVolumeString);
                 outputString.concat("\n/E\n");
             }
         }
 
-        //Notinit(初期)
+        // Notinit(初期)
         if (!isInit)
         {
-            //BOOT処理
+            // BOOT処理
             if (inputString.startsWith("/i"))
             {
                 /*
-                CH9 Nu -> CircLink
+                CH9 PCB -> IO
                     /i\n
                     /e\n
                     <<ETX>>
@@ -147,18 +156,18 @@ void loop()
             else if (inputString.startsWith("/v") || initCnt == 1)
             {
                 /*
-                CH9 Nu -> CircLink
+                CH9 PCB -> IO
                     /v\n
                     /e\n
                     <<ETX>>
                 */
-                outputString = "/I\nC:LMA (C) CAPCOM BUILD 2014/07/01\nV:REV (C) CAPCOM BUILD 2015/04/01\nF:S,?,0,0,0,00\n/E\n";
+                outputString = "/I\nC:REVIO NTKLAB LOGICBD 2021/03/28\nV:REVIO NTKLB IMITATOR 2021/03/11\nF:S,?,0,0,0,00\n/E\n";
                 initCnt++;
             }
             else if (inputString.startsWith("/s"))
             {
                 /*
-                CH9 Nu -> CircLink
+                CH9 PCB -> IO
                     /s\n
                     /e\n
                     <<ETX>>
@@ -169,7 +178,7 @@ void loop()
                 digitalWrite(LED_BUILTIN, HIGH);
 
                 /*
-                CH9 Nu -> CircLink
+                CH9 PCB -> IO
                     /s\n
                     /e\n
                     <<ETX>>
@@ -177,17 +186,17 @@ void loop()
             }
             else if (inputString.startsWith("/S"))
             {
-                //Init失敗しとるが
+                // Init失敗しとるが
                 outputString = "/S\n:BOOT\r\n/E\n";
                 isInit = true;
             }
         }
 
-        //output
+        // output
         if (outputString.length() != 0)
         {
             Serial.print(outputString);
-            Serial.write(0x3); //ETX
+            Serial.write(0x3); // ETX
         }
         inputString = "";
         outputString = "";
@@ -202,7 +211,7 @@ void serialEvent()
         uint8_t readC = Serial.read();
         inputString += (char)readC;
 
-        //ETX
+        // ETX
         if (readC == 3)
             stringComplete = true;
     }
